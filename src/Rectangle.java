fb2859f45ae2f167381d1ac20938307c39fd9179
ellykhodaie <elahekhodaie@yahoo.com>
2021-03-17 23:15:52 +0330

    added set and get for the rectangle which is needed in part 2

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,11 +2,28 @@ public class Rectangle {
     private float height;
     private float  width;
 
+    public Rectangle(float width, float height) {
+        this.height = height;
+        this.width = width;
+    }
 
     public float computeArea() {
         return this.width * this.height;
     }
 
+    public float getHeight() {
+        return height;
+    }
 
+    public float getWidth() {
+        return width;
+    }
 
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
 }
