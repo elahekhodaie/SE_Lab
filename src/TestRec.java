40cc2c6c88e5b49fe70ff3daec0dfe3f273c2c1a
ellykhodaie <elahekhodaie@yahoo.com>
2021-03-17 23:25:40 +0330

    the test rectangle tests for getting and checking the area is added to the file

---
+++ src/TestRec.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestRec {
+    Rectangle rectangle = new Rectangle(5,10);
+
+    @Test
+    public void testGetRectangleWidth() {
+        assertEquals(5, rectangle.getWidth(), 0);
+    }
+
+    @Test
+    public void testGetRectangleHeight() {
+        assertEquals(10, rectangle.getHeight(), 0);
+    }
+
+    @Test
+    public void testRectangleComputeArea() {
+        assertEquals(50, rectangle.computeArea(), 0);
+    }
+
+
+
+}
