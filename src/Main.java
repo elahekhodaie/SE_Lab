e900f90d14e7ead36c136d6693c186bade2d25d9
ellykhodaie <elahekhodaie@yahoo.com>
2021-03-17 18:11:25 +0330

    compute aread function added to main

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+
+public class Main {
+
+    int height;
+    int width;
+
+    public int computeArea(int height, int width){
+        return height * width;
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+
+	// write your code here
+    }
+}
