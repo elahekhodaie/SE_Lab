e1072146a02d7a5d6d2eab54c4b5da977851dd02
Sina <stavakkoli1999@gmail.com>
2021-03-17 23:39:18 +0330

    square width added

--- src/Square.java
+++ src/Square.java
@@ -1,9 +1,13 @@
 public class Square  {
 
     private float _height;
+    private float _width;
 
     public float get_height() {
         return _height;
     }
 
+    public float get_width() {
+        return _width;
+    }
 }
